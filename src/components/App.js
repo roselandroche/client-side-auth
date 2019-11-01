1b2738ce306ee4ab6467659e84c48641db6df9b7
roselandroche <r.landroche@gmail.com>
2019-10-31 20:32:25 -0400

    Following lecture...

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import "./App.css"
 import React from "react"
+import { Link, Route } from 'react-router-dom'; 
+
+import SignIn from './SignIn';
 
 function App() {
 	return (
 		<div className="wrapper">
-			<h1>Welcome</h1>
+			<nav>
+				<Link to='/'>Home</Link>
+				<Link to='/signin'>Sign In</Link>
+			</nav>
+
+			<Route exact path='/signin' component={SignIn} />
 		</div>
 	)
 }
---
+++ src/components/SignIn.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function SignIn(props) {
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (event) => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    return (
+        <form>
+            <input type='email' name='email' placeholder='Email' value={data.email} onChange={handleChange}/>
+            <input type='password' name='password' placeholder='Password' value={data.password} onChange={handleChange}/>
+
+            <button type='submit'>Sign In</button>
+        </form>
+    )
+}
+
+export default SignIn;
